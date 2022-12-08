87ae94c2bc5e6b8a07980c354cfa35adda796572
Unto Nordling <85550906+unielain@users.noreply.github.com>
2022-12-08 13:41:52 +0200

    newest functions added

--- test.py
+++ test.py
@@ -6,17 +6,6 @@ import functions
 
 
 
-
-from flask import Flask, request, Response
-from flask_cors import CORS
-import json
-import mysql.connector
-import functions
-
-
-
-
-
 # main
 app = Flask(__name__)
 cors = CORS(app)
@@ -26,7 +15,7 @@ app.config['CORS_HEADERS'] = 'Content type'
 # game starts:
 @app.route('/startgame')
 def startgame():
-    result = functions.start_screen()
+    result = functions.fetch_dialog('startscreen', 'Melon_Dusk')
     return result
 
 
@@ -36,22 +25,30 @@ def get_name():
     name = str(args.get('name'))
     check = functions.check_user_name(name)
     if check is True:
-        result = functions.fetch_dialog('Newgamepassword', 'Melon_Dusk')
+        result = functions.fetch_dialog('newgamepassword', 'Melon_Dusk')
     else:
-        result = 'Name not available'
+        result = functions.fetch_dialog('same_screen_name', 'Melon_Dusk')
     return result
 
-@app.route('/password')
-def get_password(uname, pword):
-    username = uname
-    password = pword
-    functions.create_new_user(username, password)
-    result = functions.fetch_dialog('newgametutorial' ,'Melon_Dusk')
+
+@app.route('/new_password')
+def create_password():
+    args = request.args
+    password = str(args.get('password'))
+    name = str(args.get('name'))
+    result = functions.fetch_dialog('newgametutorial', 'Melon_Dusk')
+    functions.create_new_user(name, password)
     return result
 
-if __name__ == '__main__':
-   app.run(use_reloader=True, host='127.0.0.1', port=5000)
 
+# gets a list of the items that need fetching
+@app.route('/list_items')
+def list_of_items():
+    items = functions.get_and_rand_object()
+    return json.dumps(items, default=lambda o: o.__dict__, indent=4)
 
-if __name__ == '__main__':
-   app.run(use_reloader=True, host='127.0.0.1', port=5000)
+@app.route('/list_locations')
+def list_of_locations():
+    items = functions.list_of_lat_long('latitude_deg', 'longitude_deg')
+    return json.dumps(items, default=lambda o: o.__dict__, indent=4)
+
