08f54964ccf65bc048740236ae576f1fb6c0929f
Unto Nordling <85550906+unielain@users.noreply.github.com>
2022-12-08 12:59:42 +0200

    Update test.py

--- test.py
+++ test.py
@@ -7,6 +7,15 @@ import functions
 
 
 
+from flask import Flask, request, Response
+from flask_cors import CORS
+import json
+import mysql.connector
+import functions
+
+
+
+
 
 # main
 app = Flask(__name__)
@@ -32,6 +41,17 @@ def get_name():
     else:
         result = 'Name not available'
     return result
+
+@app.route('/password')
+def get_password(uname, pword):
+    username = uname
+    password = pword
+    functions.create_new_user(username, password)
+    result = functions.fetch_dialog('newgametutorial' ,'Melon_Dusk')
+    return result
+
+if __name__ == '__main__':
+   app.run(use_reloader=True, host='127.0.0.1', port=5000)
 
 
 if __name__ == '__main__':
