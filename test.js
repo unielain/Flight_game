f02cea856929d90737c01f56c252376a50d78d07
Unto Nordling <85550906+unielain@users.noreply.github.com>
2022-12-11 11:19:23 +0200

    new function added
    now the story goes on

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
-
 console.log('start');
+const mapdiv = document.querySelector(`#map`);
 
 //Startscreen
 async function startscreen() {
@@ -9,8 +9,7 @@ async function startscreen() {
         if (response.ok) {
             const data = await response.json();
             console.log(data);
-            const txt = data
-            return txt;
+            return data;
         } else {
             const answer = 'No voi paska, ei toimi' + response.error;
             return answer
@@ -20,9 +19,10 @@ async function startscreen() {
         return error
     }
 }
+
 // get_value from input
 
-function getInputValue(){
+function getInputValue() {
     const input_field = document.querySelector(`input`);
     const value = input_field.value;
     return value
@@ -30,13 +30,12 @@ function getInputValue(){
 }
 
 //get_name
-async function getName(input){
+async function getName(input) {
     try {
         const response = await fetch(`http://127.0.0.1:5000/screen_name?name=${input}`);
-        if(response.ok) {
+        if (response.ok) {
             const data = await response.json();
             console.log(data);
-            const txt = data
             return data;
         }
     } catch (error) {
@@ -46,10 +45,10 @@ async function getName(input){
 }
 
 //new_userpword
-async function newPassword(password, name){
+async function newPassword(password, name) {
     try {
         const response = await fetch(`http://127.0.0.1:5000/new_password?password=${password}&name=${name}`);
-        if(response.ok) {
+        if (response.ok) {
             const data = await response.json();
             console.log(data);
             return data
@@ -61,28 +60,63 @@ async function newPassword(password, name){
 }
 
 //get list of items:
-async function getItems(){
+async function getItems() {
     try {
         const response = await fetch('http://127.0.0.1:5000/list_items');
-        if(response.ok){
+        if (response.ok) {
             const data = await response.json();
             console.log(data);
             return data
         }
 
-    } catch (error){
+    } catch (error) {
         console.log(`error`, error)
         return error
     }
 }
 
+//gets hints for items
+
+async function getHints(items) {
+    try {
+        const response = await fetch(`http://127.0.0.1:5000/get_a_hint?items=${items}`);
+        if (response.ok) {
+            const data = await response.json();
+            console.log(data);
+            return data
+        }
+
+    } catch (error) {
+        console.log(`error`, error)
+        return error
+    }
+}
+
+async function narration_continues(){
+    try {
+        const response = await fetch(`http://127.0.0.1:5000/stranger_advice`);
+    if (response.ok) {
+            const data = await response.json();
+            console.log(data);
+            return data
+        }
+
+    } catch (error) {
+        console.log(`error`, error)
+        return error
+    }
+}
+
+// gets starting location
+
+
 //main
 async function main() {
     let txt = await startscreen();
-    const start_screen = document.querySelector(`#startscreen`);
-    const paragraph = document.createElement(`p`);
+    const start_screen = document.querySelector(`#dialog`);
+    let paragraph = document.createElement(`p`);
     start_screen.appendChild(paragraph);
-    const textnode = document.createTextNode(txt);
+    let textnode = document.createTextNode(txt);
     paragraph.appendChild(textnode);
     const input = document.createElement(`input`);
     input.type = "text";
@@ -91,45 +125,56 @@ async function main() {
     const submit_name = document.createTextNode(`submit`);
     submit.appendChild(submit_name);
     start_screen.appendChild(submit);
-    submit.addEventListener(`click`, async function(event){
+    submit.addEventListener(`click`, async function (event) {
         event.preventDefault();
         const value = getInputValue();
         console.log(value);
-        const response = await getName(value);
+        let response = await getName(value);
         const username = value
         let next_txt = document.createTextNode(response);
         const input_password = document.createElement('input');
         input_password.type = "password";
-        if (response[0] === 'You need to create a password for your inventory.'){
+        if (response[0] === 'You need to create a password for your inventory.') {
+            paragraph.appendChild(next_txt);
             paragraph.replaceChild(next_txt, textnode);
             start_screen.replaceChild(input_password, input);
-            submit.addEventListener(`click`, async  function(event){
+            submit.addEventListener(`click`, async function (event) {
                 const input = getInputValue();
-                const result = await newPassword(input, username);
-                const txt = document.createTextNode(result);
+                let result = await newPassword(input, username);
+                let txt = document.createTextNode(result);
                 const items = await getItems();
                 const list_items = document.createElement(`ul`);
                 start_screen.appendChild(list_items);
-                for(let i = 0; i < items.length; i++){
-                    let item = document.createElement(`li`)
+                for (let i = 0; i < items.length; i++) {
+                    let item = document.createElement(`li`);
                     item.textContent = items[i];
-                    list_items.appendChild(item)
+                    list_items.appendChild(item);
                 }
-
                 paragraph.removeChild(next_txt);
                 paragraph.appendChild(txt);
                 start_screen.removeChild(input_password);
                 start_screen.removeChild(submit);
+                const nextDialog = document.createElement(`button`);
+                nextDialog.textContent = "next";
+                start_screen.appendChild(nextDialog);
+                nextDialog.addEventListener(`click`, async function(event){
+                   paragraph.removeChild(txt);
+                   let result = await narration_continues();
+                   let nexText = document.createTextNode(result);
+                   paragraph.appendChild(nexText);
+                });
 
 
-            })
+
+            });
         }
 
+    });
 
-    })
+
 }
-const startbutton = document.querySelector(`#start`)
-startbutton.addEventListener(`click`, async function (event) {
-    main();
 
-});
+
+main();
+
+
